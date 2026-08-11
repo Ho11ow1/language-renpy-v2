675d75b4df610074f0f0a70d45271ed389948008
Hollow1 <jakubvalesilver@gmail.com>
2026-08-12 01:40:31 +0200

    Update hover in place for now to add correct detection, probably doesn't work fully for screen lang but that's for the future

--- src/hover.ts
+++ src/hover.ts
@@ -6,10 +6,23 @@ import { Declaration } from "@models/Declaration";
 
 export class HoverItemProvider implements vscode.HoverProvider
 {
+    // Vars
     private identifierPartRegex: RegExp = /^[a-zA-Z0-9_]*/;
     private chainBeforeCursorRegex: RegExp = /[a-zA-Z_]\w*(?:\.[a-zA-Z_]\w*)*$/;
-    private imageDeclRegex: RegExp = /^\s*image\s+([a-zA-Z0-9_\s]+?)\s*[:=]/;
-    private imageUsageRegex: RegExp = /\b(?:show|scene|hide)\s+(.+?)(?=\s+at\b|$)/;
+
+    // Primitive implementation, I'll have to build the ContextParser which will handle this, additionally definition.ts also needs to be fixed so that's ContextParser also
+    // Also kind of have to handle both add img & add screen so this won't work here but should work in the context parser
+    // We probably won't be supporting everything, and at the same time for something like add we should also allow functions like FileScreenShoto() but that requires json restructure which i have yet to add
+    private imageDeclRegex: RegExp = /^\s*image\s+([a-zA-Z0-9_\s]+?)\s*[=:]\s*([\s\S]*)$/;
+    private imageUsageRegex: RegExp = /\b(?:show|scene|hide)\s+(.+?)(?:\s+at\s+.+|:)?$/;
+    private labelDeclRegex: RegExp = /^\s*label\s+(?!_\s*\()([a-zA-Z_]\w*)\s*(?:\([^)]*\))?\s*:/;
+    private labelUsageRegex: RegExp = /\b(?:jump|call)\s+([a-zA-Z0-9_]+)(?:\([^)]*\))?/;
+    private transformDeclRegex: RegExp = /^\s*transform\s+([a-zA-Z_]\w*)\s*(?:\([^)]*\))?\s*:/;
+    private transformUsageRegex: RegExp = /\b(?:show|scene|hide)(?:\s+screen)?\s+\w+(?:\s+\w+)?\s+(?:at)\s+([a-zA-Z0-9_]+)/;
+    private screenDeclRegex: RegExp = /^\s*screen\s+([a-zA-Z_]\w*)\s*(?:\([^)]*\))?\s*:/;
+    private screenUsage1Regex: RegExp = /\b(?:show|call|hide)\s+(?:screen)\s+([a-zA-Z0-9_]+)(?:\([^)]*\))?/;
+    private screenUsage2Regex: RegExp = /\b(?:add)\s+([a-zA-Z0-9_]+)(?:\([^)]*\))?$/;
+
     private renpyStorePrefix: string = "renpy.store.";
 
     public provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): vscode.ProviderResult<vscode.Hover>
@@ -31,10 +44,58 @@ export class HoverItemProvider implements vscode.HoverProvider
 
             if (offset >= imageStart && offset <= imageEnd)
             {
-                const decl = Store.getDeclarationAtPath([/*"image",*/imageName]);
+                const decl = Store.getDeclarationAtPath(["image", imageName]);
                 if (decl)
                 {
                     return this.getHoverComponent(decl, new vscode.Range(position.line, imageStart, position.line, imageEnd));
+                }
+            }
+        }
+        const labelDeclMatch = line.match(this.labelDeclRegex) ?? line.match(this.labelUsageRegex);
+        if (labelDeclMatch)
+        {
+            const labelName = labelDeclMatch[1].trim();
+            const labelStart = labelDeclMatch.index! + labelDeclMatch[0].indexOf(labelName);
+            const labelEnd = labelStart + labelName.length;
+
+            if (offset >= labelStart && offset <= labelEnd)
+            {
+                const decl = Store.getDeclarationAtPath(["label", labelName]);
+                if (decl)
+                {
+                    return this.getHoverComponent(decl, new vscode.Range(position.line, labelStart, position.line, labelEnd));
+                }
+            }
+        }
+        const transformDeclMatch = line.match(this.transformDeclRegex) ?? line.match(this.transformUsageRegex);
+        if (transformDeclMatch)
+        {
+            const transformName = transformDeclMatch[1].trim();
+            const transformStart = transformDeclMatch.index! + transformDeclMatch[0].indexOf(transformName);
+            const transformEnd = transformStart + transformName.length;
+
+            if (offset >= transformStart && offset <= transformEnd)
+            {
+                const decl = Store.getDeclarationAtPath(["transform", transformName]);
+                if (decl)
+                {
+                    return this.getHoverComponent(decl, new vscode.Range(position.line, transformStart, position.line, transformEnd));
+                }
+            }
+        }
+        const screenDeclMatch = line.match(this.screenDeclRegex) ?? line.match(this.screenUsage1Regex) ?? line.match(this.screenUsage2Regex);
+        if (screenDeclMatch)
+        {
+            const screenName = screenDeclMatch[1].trim();
+            const screenStart = screenDeclMatch.index! + screenDeclMatch[0].indexOf(screenName);
+            const screenEnd = screenStart + screenName.length;
+
+            if (offset >= screenStart && offset <= screenEnd)
+            {
+                const decl = Store.getDeclarationAtPath(["screen", screenName]);
+                if (decl)
+                {
+                    return this.getHoverComponent(decl, new vscode.Range(position.line, screenStart, position.line, screenEnd));
                 }
             }
         }
