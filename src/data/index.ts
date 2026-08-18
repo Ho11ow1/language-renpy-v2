9ced5b11512e657cc3fd734f4bec22bc8acc4876
Hollow1 <jakubvalesilver@gmail.com>
2026-08-18 19:55:26 +0200

    Finally do the big json restructuring cleanup, start organizingthings with index.ts modules, more context in next commit as i forgot to commit things from sleep deprevation

---
+++ src/data/index.ts
@@ -0,0 +1,3 @@
+export { pythonRootFunctions, pythonTypeMethods, limitedPythonRootFunctions } from "./python";
+export { renpyImages, renpyRootActions, renpyRootClasses, renpyRootFunctions, renpyRootMatrixes, renpyTransforms, renpyTransitions } from "./renpy";
+export { default as renpyJson } from "./renpy.json";
